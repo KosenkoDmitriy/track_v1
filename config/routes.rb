c024e7b0d8636fa5cfb7efd037e29b58f0071aee
Shopomob <sm@62-210-170-178.online.net shopomob.ru adec.su>
2019-10-11 10:30:05 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Track::Application.routes.draw do
 
-  # This line mounts Spree's routes at the root of your application.
-  # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
-  # If you would like to change where this engine is mounted, simply change the :at option to something different.
-  #
-  # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-
   get "maildev" => 'emaildev#index'
 
   get "welcome" => "application#welcome", path: "/shop/welcome/"
